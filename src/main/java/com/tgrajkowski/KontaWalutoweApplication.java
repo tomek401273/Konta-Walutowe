edb95a223e715df90448fb3747248269229c9819
tomek401273 <tomek371240@gmail.com>
2022-09-20 21:26:55 +0200

    init commit

---
+++ src/main/java/com/tgrajkowski/KontaWalutoweApplication.java
@@ -0,0 +1,13 @@
+package com.tgrajkowski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KontaWalutoweApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KontaWalutoweApplication.class, args);
+	}
+
+}
